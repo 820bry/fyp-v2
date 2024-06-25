b33aa1491feb872aaa4a287065a898f19e1f5fe2
Bryan Lai <bryanlai820@gmail.com>
2024-06-25 21:46:06 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// pwa
+import { VitePWA } from "vite-plugin-pwa"
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), 
+    VitePWA({
+      registerType:'autoUpdate',
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest:{
+        name:"MentalQuest - Mental Health Support System",
+        short_name:"MentalQuest",
+        description:"A Mental Health Support System for a Final Year Project",
+        theme_color: "#c9f4e4",
+        display: "standalone",  
+        icons:[
+          {  
+            src: '/pwa-192x192.png',  
+            sizes: '192x192',  
+            type: 'image/png',  
+          },  
+          {  
+            src: '/pwa-512x512.png',  
+            sizes: '512x512',  
+            type: 'image/png',  
+          },  
+          {  
+            src: '/pwa-512x512.png',  
+            sizes: '512x512',  
+            type: 'image/png',  
+            purpose: 'maskable',  
+          },],
+      }
+    })],
+})
