1bbc08537816d79cff9699b9edb3a6f0b4274b62
Bryan Lai <bryanlai820@gmail.com>
2024-06-25 22:10:50 +0800

    updated app assets

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5n6o4ga33s"
+    "revision": "0.ugk3qbvsr9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
