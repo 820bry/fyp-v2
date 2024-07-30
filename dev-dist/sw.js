fa9a648777667444392561d5b08153e960666e63
Bryan Lai <bryanlai820@gmail.com>
2024-07-30 13:38:03 +0800

    remove t&c text from register page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s1pvkkr9v5g"
+    "revision": "0.g72f4m5j8ao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
